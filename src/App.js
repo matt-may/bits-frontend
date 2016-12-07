6eb8c6ed9bd14f095dd54926776f88d61311562b
Matt May <matthewbentonmay@gmail.com>
2016-12-06 21:01:28 -0600

    work on infinitescroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import './App.css'
+import ReactDOM from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Infinite from 'react-infinite';
+import './App.css';
 
-const Bits = [
+const bits = [
   { body: 'My fair body' },
   { body: 'Hey there buddy'}
-]
+];
 
 class SearchBar extends Component {
   render() {
@@ -26,8 +28,69 @@ class BitEditor extends Component {
 class BitPreview extends Component {
   render() {
     return (
-      <div></div>
+      <div className="infinite-list-item">
+        {this.props.body}
+      </div>
     );
+  }
+}
+
+class BitBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      elements: this.buildElements(0, 20),
+      isInfiniteLoading: false
+    };
+  }
+
+  buildElements(start, end) {
+      var elements = [];
+
+      for (var i = start; i < end; i++) {
+          elements.push(<BitPreview key={i} num={i} body={bits[0].body}/>)
+      }
+
+      return elements;
+  }
+
+  handleInfiniteLoad = () => {
+      var that = this;
+
+      this.setState({
+          isInfiniteLoading: true
+      });
+
+      setTimeout(function() {
+        var elemLength = that.state.elements.length,
+            newElements = that.buildElements(elemLength, elemLength + 1000);
+
+        that.setState({
+            isInfiniteLoading: false,
+            elements: that.state.elements.concat(newElements)
+        });
+      }, 0);
+  }
+
+  elementInfiniteLoad() {
+      return (
+        <div className="infinite-list-item">
+          Loading...
+        </div>
+      );
+  }
+
+  render() {
+      return (
+        <Infinite elementHeight={40}
+                  containerHeight={250}
+                  infiniteLoadBeginEdgeOffset={200}
+                  onInfiniteLoad={this.handleInfiniteLoad}
+                  loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                  isInfiniteLoading={this.state.isInfiniteLoading}>
+          {this.state.elements}
+      </Infinite>);
   }
 }
 
@@ -35,7 +98,9 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <button>Clickme</button>
+        <div className="infinite">
+          <BitBox />
+        </div>
       </div>
     );
   }
