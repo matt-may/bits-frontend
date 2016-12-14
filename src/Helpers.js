4a3d22d0e4569bdd4b2ebd5efd0a18054d870bf7
Matt May <matthewbentonmay@gmail.com>
2016-12-14 11:27:26 -0600

    small helper update

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,15 +1,12 @@
 import constants from './constants';
 
-function checkStatus(response, failHandler) {
+function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
   else {
     let error = new Error(response.statusText);
     error.response = response;
-
-    if (failHandler)
-      failHandler();
 
     throw error
   }
