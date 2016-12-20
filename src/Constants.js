4331f652fa75d544c8975356894e8598dbf61ee5
Matt May <matthewbentonmay@gmail.com>
2016-12-19 18:41:05 -0600

    small fix to constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,11 @@
-const BASE_URI = 'https://bitsy.pro/api';
+const IN_PRODUCTION = !!document.getElementById('auth_token');
+const BASE_URI = IN_PRODUCTION ? 'https://bitsy.pro/api' : 'http://localhost:3005/api';
 
 const CONSTANTS = {
   BITS_PATH: BASE_URI + '/bits',
   BITS_SEARCH_PATH: BASE_URI + '/bits/search',
   USER_SHOW_PATH: BASE_URI + '/users/show',
-  IN_PRODUCTION: !!document.getElementById('auth_token')
+  IN_PRODUCTION: IN_PRODUCTION
 };
 
 export default CONSTANTS;
